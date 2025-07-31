04fc672f74aef7132aa46e5e0c35ffc6637afc07
Gilson Nunes <GilsonNunes_dev@outlook.com>
2025-07-31 14:43:37 -0300

    fix(main.ts): correct application boot

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { HTTPServer } from './server';
 import { fastifyServer } from './server/fastify';
 
 async function bootstrap() {
-	const { fastify } = new HTTPServer(fastifyServer);
-	await new Application(fastify).run();
+	const server = new HTTPServer(fastifyServer);
+	await new Application(server).run();
 }
 
 bootstrap();
